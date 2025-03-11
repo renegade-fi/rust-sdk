2fd2cf887447d99aa0fe8193a8060a71f34b9f50
Joey Kraut <joey@renegade.fi>
2025-03-10 17:09:15 -0700

    examples: Update examples to use `alloy`

--- examples/external_match/modify_order_after_quote.rs
+++ examples/external_match/modify_order_after_quote.rs
@@ -1,24 +1,15 @@
 //! An example in which we tweak an order before assembling the quote
 
-use std::{str::FromStr, sync::Arc};
-
-use ethers::middleware::Middleware;
-use ethers::prelude::*;
 use renegade_sdk::{
-    types::{AtomicMatchApiBundle, ExternalOrder, OrderSide},
+    example_utils::{build_renegade_client, execute_bundle, get_signer, Wallet},
+    types::{ExternalOrder, OrderSide},
     AssembleQuoteOptions, ExternalMatchClient, ExternalOrderBuilder,
 };
-
-/// The RPC URL to use
-const RPC_URL: &str = env!("RPC_URL");
 
 /// Testnet wETH
 const BASE_MINT: &str = "0xc3414a7ef14aaaa9c4522dfc00a4e66e74e9c25a";
 /// Testnet USDC
 const QUOTE_MINT: &str = "0xdf8d259c04020562717557f2b5a3cf28e92707d1";
-
-/// The middleware type
-type Wallet = Arc<SignerMiddleware<Provider<Http>, LocalWallet>>;
 
 #[tokio::main]
 async fn main() -> Result<(), eyre::Error> {
@@ -26,10 +17,7 @@ async fn main() -> Result<(), eyre::Error> {
     let signer = get_signer().await?;
 
     // Get the external match client
-    let api_key = std::env::var("EXTERNAL_MATCH_KEY").unwrap();
-    let api_secret = std::env::var("EXTERNAL_MATCH_SECRET").unwrap();
-    let client = ExternalMatchClient::new_sepolia_client(&api_key, &api_secret).unwrap();
-
+    let client = build_renegade_client()?;
     let order = ExternalOrderBuilder::new()
         .base_mint(BASE_MINT)
         .quote_mint(QUOTE_MINT)
@@ -70,29 +58,4 @@ async fn fetch_quote_and_execute(
         None => eyre::bail!("No bundle found"),
     };
     execute_bundle(wallet, resp.match_bundle).await
-}
-
-/// Execute a bundle directly
-async fn execute_bundle(wallet: &Wallet, bundle: AtomicMatchApiBundle) -> Result<(), eyre::Error> {
-    println!("Submitting bundle...\n");
-    let tx = bundle.settlement_tx.clone();
-    let receipt: PendingTransaction<_> = wallet.send_transaction(tx, None).await.unwrap();
-
-    println!("Successfully submitted transaction: {:#x}", receipt.tx_hash());
-    Ok(())
-}
-
-// -----------
-// | Helpers |
-// -----------
-
-/// Get a wallet from a private key environment variable
-async fn get_signer() -> Result<Wallet, eyre::Error> {
-    let provider = Provider::<Http>::try_from(RPC_URL).unwrap();
-    let chain_id = provider.get_chainid().await.unwrap().as_u64();
-    let pkey = std::env::var("PKEY").unwrap();
-    let wallet = LocalWallet::from_str(&pkey).unwrap().with_chain_id(chain_id);
-    let middleware = Arc::new(SignerMiddleware::new(provider, wallet));
-
-    Ok(middleware)
 }
