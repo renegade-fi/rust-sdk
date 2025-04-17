803680c600fdaaf3c324a7998d2d7af67d0d9dc3
Joey Kraut <108701651+joeykraut@users.noreply.github.com>
2025-04-16 19:31:05 -0700

    external-match-client: api-types, client: Expose `allow_shared` option (#16)

---
+++ examples/external_match/shared_bundle.rs
@@ -0,0 +1,55 @@
+use renegade_sdk::example_utils::{build_renegade_client, execute_bundle, get_signer, Wallet};
+use renegade_sdk::AssembleQuoteOptions;
+use renegade_sdk::{
+    types::{ExternalOrder, OrderSide},
+    ExternalMatchClient, ExternalOrderBuilder,
+};
+
+/// Testnet wETH
+const BASE_MINT: &str = "0xc3414a7ef14aaaa9c4522dfc00a4e66e74e9c25a";
+/// Testnet USDC
+const QUOTE_MINT: &str = "0xdf8d259c04020562717557f2b5a3cf28e92707d1";
+
+#[tokio::main]
+async fn main() -> Result<(), eyre::Error> {
+    // Get wallet from private key
+    let signer = get_signer().await?;
+
+    // Get the external match client
+    let client = build_renegade_client()?;
+    let order = ExternalOrderBuilder::new()
+        .base_mint(BASE_MINT)
+        .quote_mint(QUOTE_MINT)
+        .quote_amount(30_000_000) // $30 USDC
+        .min_fill_size(30_000_000) // $30 USDC
+        .side(OrderSide::Sell)
+        .build()
+        .unwrap();
+
+    fetch_quote_and_execute(&client, order, &signer).await?;
+    Ok(())
+}
+
+/// Fetch a quote from the external api and print it
+async fn fetch_quote_and_execute(
+    client: &ExternalMatchClient,
+    order: ExternalOrder,
+    wallet: &Wallet,
+) -> Result<(), eyre::Error> {
+    // Fetch a quote from the relayer
+    println!("Fetching quote...");
+    let res = client.request_quote(order.clone()).await?;
+    let quote = match res {
+        Some(quote) => quote,
+        None => eyre::bail!("No quote found"),
+    };
+
+    // Assemble the quote into a bundle
+    println!("Assembling quote...");
+    let options = AssembleQuoteOptions::new().with_allow_shared(true);
+    let resp = match client.assemble_quote_with_options(quote, options).await? {
+        Some(resp) => resp,
+        None => eyre::bail!("No bundle found"),
+    };
+    execute_bundle(wallet, resp.match_bundle).await
+}
