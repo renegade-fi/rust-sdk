9a9695dc768c17be77f7bb11d4df06f44d1963f7
Joey Kraut <108701651+joeykraut@users.noreply.github.com>
2025-06-10 16:33:54 -0700

    external-match-client: Add all pairs book depth method (#31)

--- src/external_match_client/api_types/order_book.rs
+++ src/external_match_client/api_types/order_book.rs
@@ -4,13 +4,30 @@ use serde::{Deserialize, Serialize};
 /// Response for the GET /order_book/depth/:mint route
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct GetDepthByMintResponse {
+    /// The liquidity depth for the given mint
+    #[serde(flatten)]
+    pub depth: PriceAndDepth,
+}
+
+/// Response for the GET /order_book/depth route
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct GetDepthForAllPairsResponse {
+    /// The liquidity depth for all supported pairs
+    pub pairs: Vec<PriceAndDepth>,
+}
+
+/// The liquidity depth for a given pair
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct PriceAndDepth {
+    /// The token address
+    pub address: String,
     /// The current price of the token in USD
     pub price: f64,
     /// The timestamp of the price
     pub timestamp: u64,
-    /// The buy side depth
+    /// The liquidity depth for the buy side
     pub buy: DepthSide,
-    /// The sell side depth
+    /// The liquidity depth for the sell side
     pub sell: DepthSide,
 }
 
