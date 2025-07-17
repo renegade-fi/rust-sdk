f048890a58086e938ce9473d88831a239a344c27
Joey Kraut <joey@renegade.fi>
2025-07-17 16:24:10 -0700

    renegade-wallet-client: actions: cancel-order: Add order cancellation

---
+++ src/renegade_wallet_client/actions/cancel_order.rs
@@ -0,0 +1,31 @@
+//! Cancels an order in the wallet
+
+use renegade_api::http::wallet::{CancelOrderRequest, CancelOrderResponse, CANCEL_ORDER_ROUTE};
+use uuid::Uuid;
+
+use crate::{
+    actions::{construct_http_path, prepare_wallet_update},
+    client::RenegadeClient,
+    RenegadeClientError,
+};
+
+impl RenegadeClient {
+    /// Cancels an order in the wallet
+    pub async fn cancel_order(&self, order_id: Uuid) -> Result<(), RenegadeClientError> {
+        let mut wallet = self.get_internal_wallet().await?;
+        if !wallet.contains_order(&order_id) {
+            return Err(RenegadeClientError::wallet(format!(
+                "Order {order_id} not found in wallet"
+            )));
+        }
+
+        wallet.remove_order(&order_id).unwrap();
+        let update_auth = prepare_wallet_update(&mut wallet)?;
+
+        // Send the request
+        let route = construct_http_path!(CANCEL_ORDER_ROUTE, "wallet_id" => self.secrets.wallet_id, "order_id" => order_id);
+        let request = CancelOrderRequest { update_auth };
+        let _response: CancelOrderResponse = self.post_relayer(&route, request).await?;
+        Ok(())
+    }
+}
