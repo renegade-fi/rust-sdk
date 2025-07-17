e14ffed482dba5b3bcfbb3ed5f6d4b577e464859
Joey Kraut <joey@renegade.fi>
2025-07-17 14:30:34 -0700

    renegade-wallet-client: actions: get-wallet: Add get wallet command

--- src/renegade_wallet_client/actions/mod.rs
+++ src/renegade_wallet_client/actions/mod.rs
@@ -1,3 +1,17 @@
 //! Actions to update a Renegade wallet
 
 pub mod create_wallet;
+pub mod get_wallet;
+
+/// Constructs an HTTP path by replacing URL parameters with given values
+macro_rules! construct_http_path {
+    ($base_url:expr $(, $param:expr => $value:expr)*) => {{
+        let mut url = $base_url.to_string();
+        $(
+            let placeholder = format!(":{}", $param);
+            url = url.replace(&placeholder, &$value.to_string());
+        )*
+        url
+    }};
+}
+pub(crate) use construct_http_path;
