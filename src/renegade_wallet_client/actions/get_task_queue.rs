10ba850e887b1f9106e1e1b84021011800eb9c4e
Andrew Kirillov <20803092+akirillo@users.noreply.github.com>
2025-09-11 14:10:01 -0700

    renegade_wallet_client: actions: add get_task_queue action (#44)

---
+++ src/renegade_wallet_client/actions/get_task_queue.rs
@@ -0,0 +1,18 @@
+//! Fetches the wallet's current task queue from the relayer
+
+use renegade_api::http::task::{ApiTaskStatus, TaskQueueListResponse, GET_TASK_QUEUE_ROUTE};
+
+use crate::{actions::construct_http_path, client::RenegadeClient, RenegadeClientError};
+
+impl RenegadeClient {
+    /// Fetches the list of tasks currently enqueued for the wallet by the
+    /// relayer.
+    ///
+    /// This includes the currently-running task, if there is one.
+    pub async fn get_task_queue(&self) -> Result<Vec<ApiTaskStatus>, RenegadeClientError> {
+        let wallet_id = self.secrets.wallet_id;
+        let path = construct_http_path!(GET_TASK_QUEUE_ROUTE, "wallet_id" => wallet_id);
+        let response: TaskQueueListResponse = self.get_relayer(&path).await?;
+        Ok(response.tasks)
+    }
+}
